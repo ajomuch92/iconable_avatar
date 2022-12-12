1e3ef3d86ca185169080f514f9463549d28348b0
ajomuch92 <aaronmontes_92@yahoo.es>
2022-12-11 20:21:08 -0600

    Adding main library files

---
+++ lib/SegmentCircle.dart
@@ -0,0 +1,102 @@
+library iconable_avatar;
+
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+/// Class to generate the segment for the bottom container
+class SegmentCircle extends CustomClipper<Path> {
+  /// double value for circle avatar radius
+  final double radius;
+
+  /// List with points on axie X
+  List<double> _xCoordinates = [];
+
+  /// List with the value of the coordinate x,y to draw the custom clipper
+  final List<List<double>> _coordinates = [];
+
+  SegmentCircle({this.radius = 0});
+
+  @override
+  Path getClip(Size size) {
+    double width = size.width;
+    double height = size.height;
+    double delta = width / 5000;
+    if (_xCoordinates.isEmpty) {
+      _xCoordinates = List.generate(5000, (index) => delta * (index + 1));
+    }
+    if (_coordinates.isEmpty) {
+      List<double> hk = _findCircle(0, 0, width, 0, width / 2, height);
+      for(double x in _xCoordinates) {
+      double y = _evaluateCircleFunction(height, width, x, hk[0], hk[1]);
+      _coordinates.add([x,y]);
+    }
+    }
+    Path path = Path();
+    for(List<double> coordinate in _coordinates) {
+      path.lineTo(coordinate[0], coordinate[1]);
+    }
+    path.close();
+    return path;
+  }
+
+  /// method to evaluate a X coordinate and generate the Y coordinate
+  double _evaluateCircleFunction(double height, double width, double x, double h, double k) {
+    double y = sqrt(pow(radius, 2) - pow(x - h, 2)) + k;
+    return y;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => true;
+
+  /// Method to calculate the values used as h and k to generate the function to draw the circle
+  List<double> _findCircle(double x1, double y1, double x2, double y2, double x3, double y3)
+  {
+    double x12 = x1 - x2;
+    double x13 = x1 - x3;
+
+    double y12 = y1 - y2;
+    double y13 = y1 - y3;
+
+    double y31 = y3 - y1;
+    double y21 = y2 - y1;
+
+    double x31 = x3 - x1;
+    double x21 = x2 - x1;
+
+    // x1^2 - x3^2
+    double sx13 = (pow(x1, 2) - pow(x3, 2)) as double;
+
+    // y1^2 - y3^2
+    double sy13 = (pow(y1, 2) - pow(y3, 2)) as double;
+
+    double sx21 = (pow(x2, 2) - pow(x1, 2)) as double;
+            
+    double sy21 = (pow(y2, 2) - pow(y1, 2)) as double;
+
+    double f = ((sx13) * (x12)
+        + (sy13) * (x12)
+        + (sx21) * (x13)
+        + (sy21) * (x13))
+        / (2 * ((y31) * (x12) - (y21) * (x13)));
+    double g = ((sx13) * (y12)
+        + (sy13) * (y12)
+        + (sx21) * (y13)
+        + (sy21) * (y13))
+        / (2 * ((x31) * (y12) - (x21) * (y13)));
+
+    double c = -pow(x1, 2) - pow(y1, 2) - 2 * g * x1 - 2 * f * y1;
+
+    // eqn of circle be x^2 + y^2 + 2*g*x + 2*f*y + c = 0
+    // where centre is (h = -g, k = -f) and radius r
+    // as r^2 = h^2 + k^2 - c
+    double h = -g;
+    double k = -f;
+    //double sqr_of_r = h * h + k * k - c;
+
+    // r is the radius
+    // double r = sqrt(sqr_of_r);
+    return [h, k];
+  }
+
+
+}
--- lib/iconable_avatar.dart
+++ lib/iconable_avatar.dart
@@ -1,3 +1,99 @@
 library iconable_avatar;
 
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'SegmentCircle.dart';
 
+/// Widget to show an avatar with a bottom section with a tappable icon
+class IconableAvatar extends StatefulWidget {
+  /// Double value to indicate the avatar radius. Make sure to use the same avatar radius
+  final double radius;
+
+  /// Widget to use as avatar, make sure to use an avatar, if not, the widget won't work
+  final Widget avatar;
+
+  /// Bool value to indicate when to show or hide the bottom section
+  final bool? iconVisible;
+
+  /// Callback use when tap on icon
+  final VoidCallback? onIconTap;
+
+  /// Icondata to show on the bottom section. By default is Icons.camera_alt
+  final IconData? icon;
+
+  /// Color used as icon color
+  final Color? iconColor;
+
+  /// Color used as background for bottom section
+  final Color? backgroundIconColor;
+
+  IconableAvatar({
+    Key? key, 
+    required this.radius,
+    required this.avatar, 
+    this.iconVisible = true, 
+    this.onIconTap,
+    this.icon = Icons.camera_alt,
+    this.iconColor = Colors.black,
+    this.backgroundIconColor = const Color.fromRGBO(0, 0, 0, 0.3),
+  }) : super(key: key) {
+    assert(avatar is CircleAvatar);
+    CircleAvatar _avatar = avatar as CircleAvatar;
+    assert(_avatar.radius == radius);
+  }
+
+  @override
+  State<IconableAvatar> createState() => _IconableAvatarState();
+}
+
+class _IconableAvatarState extends State<IconableAvatar> {
+  late double diameter, radius, width, height;
+
+  @override
+  void initState() {
+    super.initState();
+    radius = widget.radius;
+    diameter = radius * 2;
+    height = diameter / 5;
+    _getWidth();
+  }
+
+  /// Method to get the width for bottom container based on radius
+  void _getWidth() {
+    double y = (diameter - diameter / 5) / 2;
+    double x = sqrt(pow(radius, 2) - pow(y, 2));
+    double distance = radius - x;
+    double widthCalculated = diameter - distance;
+    setState(() {
+      width = widthCalculated;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.loose,
+      clipBehavior: Clip.none,
+      alignment: Alignment.center,
+      children: [
+        widget.avatar,
+        Positioned(
+          bottom: 0,
+          child: ClipPath(
+            clipper: SegmentCircle(radius: radius),
+            child: AnimatedContainer(
+              color: widget.backgroundIconColor,
+              duration: const Duration(milliseconds: 200),
+              width: width,
+              height: widget.iconVisible! ? height : 0,
+              child: IconButton(
+                icon: Icon(widget.icon, size: height * 0.85, color: widget.iconColor,),
+                onPressed: widget.onIconTap,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
